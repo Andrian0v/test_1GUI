232475d574a2cd3e814cb6ecff9919e2bb10aaa5
Andrian0v <112649696+Andrian0v@users.noreply.github.com>
2023-09-28 15:40:45 +0300

    1
    1

---
+++ Assets/SettingsPopup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class SettingsPopup : MonoBehaviour {
+	public void Open() {
+		gameObject.SetActive(true);
+	}
+	public void OnSubmitName(string name) 
+	{
+		Debug.Log(name);
+	}
+	public void OnSpeedValue(float speed) {
+		Messenger<float>.Broadcast(GameEvent.SPEED_CHANGED, speed);
+	}
+	public void Close() {
+		gameObject.SetActive(false);
+	}
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class UIController : MonoBehaviour 
+{
+	[SerializeField] private Text scoreLabel;
+	[SerializeField] private SettingsPopup settingsPopup;
+	void Start() 
+	{
+		_score = 0;
+		scoreLabel.text = _score.ToString();
+		settingsPopup.Close();
+	}
+	public void OnPointerDown() 
+	{
+		Debug.Log("pointer down");
+	}
+	private int _score;
+	void Awake() {
+		Messenger.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+	}
+	void OnDestroy() {
+		Messenger.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+	}
+	private void OnEnemyHit() {
+		_score += 1;
+		scoreLabel.text = _score.ToString();
+	}
+	public void OnOpenSettings() 
+	{ 
+		settingsPopup.Open(); 
+	}
+}
